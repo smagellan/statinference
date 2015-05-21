7d877bdfd8472af173dcd886abbbf0bf235da002
Vladimir <vladimir@fankyfranky>
2015-05-22 01:31:38 +0300

    work in progress

--- subproject1.R
+++ subproject1.R
@@ -1,13 +1,15 @@
-lambdaCoeff  <- 0.2;
-nCoeff       <- 40;
-nSimulations <- 1000;
-distrMean    <- 1 / lambdaCoeff;
-distrStdDev  <- distrMean;
+lambdaCoeff   <- 0.2;
+nCoeff        <- 40;
+nSimulations  <- 1000;
 
+#distribution of exponential: mean
+distrMean     <- 1 / lambdaCoeff;
+#distribution of exponential: standard deviation
+distrStdDev   <- 1 / lambdaCoeff;
+#distribution of exponential: variance
+distrVariance <- distrStdDev ^ 2;
 
 hist(rexp(nCoeff, lambdaCoeff));
-distrMean
-distrStdDev
 readline();
 mns <- NULL;
 for (i in 1 : nSimulations){
@@ -15,3 +17,11 @@ for (i in 1 : nSimulations){
 }
 hist(mns);
 
+#distribution of averages of 40 exponentials: theoretical mean
+
+
+
+#distribution of averages of 40 exponentials: theoretical variance
+f2TVariance <- distrVariance / nCoeff;
+
+print(sprintf("f2Variance: %f", f2Variance));
